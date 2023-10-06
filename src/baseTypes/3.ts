7c0daf467e1be57afbe4178b5289eeecece5e9b6
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-10-07 00:06:59 +0300

    done tasks 2 3 4 5

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,15 @@
-let some:unknown;
-some = 'Text';
+// У TypeScript тип unknown дозволяє нам зберігати будь-які значення, але ми можемо привласнити unknown змінну безпосередньо інший змінної, якщо ми впевнені у її типі. У вас є наступний код:
+// let some: unknown;
+// some = "Text";
+// let str: string;
+// str = some;
+// Що потрібно виправити в цьому коді, щоб він став правильним та безпечним?
+
+let some: unknown;
+some = "Text";
 let str: string;
-str = some;
+if (typeof some === "string") {
+  str = some;
+}
 
 export {};
